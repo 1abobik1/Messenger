4ac9d88d40259590bbc355c0c55b37e3ee090099
1abobik1 <125189134+1abobik1@users.noreply.github.com>
2024-03-26 15:22:54 +0300

    new code

--- server.h
+++ server.h
@@ -3,113 +3,128 @@
 #include <nlohmann/json.hpp>
 #include <uwebsockets/App.h>
 #include <iostream>
+#include<vector>
+#include<thread>
+#include<algorithm>
+
 #include "CommonConst.h"
 
 using json = nlohmann::json;
 
-// пользователь
 struct UserData
 {
 	uint64_t user_id;
 	std::string name = "NO_NAME";
 };
 
-//id пользовател€
-inline uint32_t cnt_user = 1;
+//id пользовател€(атомарный дл€ многопоточности)
+inline std::atomic_ulong cnt_user = 1;
 
 inline void runServer(const uint16_t port)
 {
 	using namespace JsonObj; using namespace PathHTML;
 
-	// /* - значит что запускаем в корнейвой папке, пока без url
-	uWS::App().ws<UserData>("/*", {
-			.idleTimeout = 960,
+    std::vector<std::thread*> threads(std::thread::hardware_concurrency());
+    std::ranges::transform(threads, threads.begin(), [&](auto* thr)
+    {
+	    return new std::thread([&]()
+	    {
+	        // /* - значит что запускаем в корневой папке, пока без url
+	        uWS::App().ws<UserData>("/*", {
+	            .idleTimeout = 960,
 
-			.open = [](auto* ws)
-			{
-				UserData* data = ws->getUserData();
-				data->user_id = cnt_user++;
+	            .open = [&](auto* ws)
+	            {
+	              UserData* data = ws->getUserData();
+	              data->user_id = cnt_user++;
 
-				std::cout << "New user connected ID: " << data->user_id << "\n";
+	              std::cout << "New user connected ID: " << data->user_id << "\n";
 
-				// подключаем человека личному каналу(личные сообщени€)
-				ws->subscribe("userN" + std::to_string(data->user_id));
+	              // подключаем человека к личному каналу(дл€ личных сообщений)
+	              ws->subscribe("userN" + std::to_string(data->user_id));
 
-				//подключаем на общий канал(общий чат)
-				ws->subscribe("public + ' ' + chat");
-			},
+	              // подключаем к общему каналу(дл€ общего чата)
+	              ws->subscribe("public + ' ' + chat");
+	            },
 
-			.message = [](auto* ws, std::string_view message, uWS::OpCode)
-			{
-				UserData* data = ws->getUserData();
+	            .message = [](auto* ws, std::string_view message, uWS::OpCode)
+	            {
+	              UserData* data = ws->getUserData();
 
-				std::cout << "message from user ID: " << data->user_id << "--message: " << message << "\n";
+	              std::cout << "message from user ID: " << data->user_id << "--message: " << message << "\n";
 
-				auto parsed = json::parse(message);
+	              auto parsed = json::parse(message);
 
-				if (parsed[COMMAND] == PRIVATE_MSG)
-				{
-					const uint64_t user_id_to = parsed[RECEIVER_ID];
-					std::string user_msg = parsed[MESSAGE];
+	              if (parsed[COMMAND] == PRIVATE_MSG)
+	              {
+	                const uint64_t user_id_to = parsed[RECEIVER_ID];
+	                std::string user_msg = parsed[MESSAGE];
 
-					json response; // ответ получателю
+	                json response; // создаем ответ получателю
 
-					if (data->name != "NO_NAME")
-					{
-						response[COMMAND] = PRIVATE_MSG;
-						response[MESSAGE] = user_msg;
-						response[USER_ID_FROM] = data->name;
-					}
+	                if (data->name != "NO_NAME")
+	                {
+	                  response[COMMAND] = PRIVATE_MSG;
+	                  response[MESSAGE] = user_msg;
+	                  response[USER_ID_FROM] = data->name;
+	                }
 
-					else
-					{
-						response[COMMAND] = PRIVATE_MSG;
-						response[MESSAGE] = user_msg;
-						response[USER_ID_FROM] = data->user_id;
+	                else
+	                {
+	                  response[COMMAND] = PRIVATE_MSG;
+	                  response[MESSAGE] = user_msg;
+	                  response[USER_ID_FROM] = data->user_id;
 
-					}
+	                }
 
-					ws->publish("userN" + std::to_string(user_id_to), response.dump()); //отправл€ем в приватный канал.
-					//response.dump() отправл€ет пользователю сообщение
-				}
+	                ws->publish("userN" + std::to_string(user_id_to), response.dump()); // отправка сообщени€
+	                //response.dump() отправл€ет пользователю сообщение
+	              }
 
-				if (parsed[COMMAND] == PUBLIC_MSG)
-				{
-					std::string user_msg = parsed[MESSAGE];
+	              if (parsed[COMMAND] == PUBLIC_MSG)
+	              {
+	                std::string user_msg = parsed[MESSAGE];
 
-					json response; // ответ всем
+	                json response; // создаем ответ дл€ общего чата
 
-					if (data->name != "NO_NAME")
-					{
-						response[COMMAND] = PRIVATE_MSG;
-						response[MESSAGE] = user_msg;
-						response[USER_ID_FROM] = data->name;
-					}
+	                if (data->name != "NO_NAME")
+	                {
+	                  response[COMMAND] = PRIVATE_MSG;
+	                  response[MESSAGE] = user_msg;
+	                  response[USER_ID_FROM] = data->name;
+	                }
 
-					else
-					{
-						response[COMMAND] = PRIVATE_MSG;
-						response[MESSAGE] = user_msg;
-						response[USER_ID_FROM] = data->user_id;
-					}
+	                else
+	                {
+	                  response[COMMAND] = PRIVATE_MSG;
+	                  response[MESSAGE] = user_msg;
+	                  response[USER_ID_FROM] = data->user_id;
+	                }
 
-					ws->publish("public + ' ' + chat", response.dump()); // отправл€ем в общий канал
-				}
+	                ws->publish("public + ' ' + chat", response.dump()); // отправка сообщени€
+	              }
 
-				if (parsed[COMMAND] == SET_NAME)
-				{
-					data->name = parsed[NEW_NAME];
-				}
-			},
+	              if (parsed[COMMAND] == SET_NAME)
+	              {
+	                data->name = parsed[NEW_NAME];
+	              }
+	            },
 
-			.close = [](auto* ws, int code, std::string_view message)
-			{
-				std::cout << "User disconnected ID: " << ws->getUserData()->user_id << "\n";
-			}
+	            .close = [](auto* ws, int code, std::string_view message)
+	            {
+	              std::cout << "User disconnected ID: " << ws->getUserData()->user_id << "\n";
+	            }
 
-		}).listen(port, [](auto*)
-			{
-				std::cout << "Listening for connections..." << "\n";
+	            }).listen(port, [](auto* token)
+	                {
+	                    assert(token, "server startup error");
+	                    std::cout << "Listening for connections..." << "\n";
+	                }).run();
+	    });
+    });
 
-			}).run();
+    for (const auto thread : threads)
+    {
+        thread->join();
+    }
 }
