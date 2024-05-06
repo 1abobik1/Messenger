dc1a4150c7ccb1c6b699de3558106fce32ee26f6
Juryxa <slavochka_kondratev80@mail.ru>
2024-05-06 19:44:34 +0300

    fixed client routing, fixed webSockets, index.html updated

--- front-end/src/components/BurgerMenu.jsx
+++ front-end/src/components/BurgerMenu.jsx
@@ -5,15 +5,13 @@ import {Link, useNavigate} from "react-router-dom";
 
 const BurgerMenu = ({active, setActive}) => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [searchResult, setSearchResult] = useState(['krasava', 'legenda krasava', 'aye', 'lox_pidor', 'legenda', 'legenda', 'legenda', 'legenda']);
+  const [searchResult, setSearchResult] = useState({user_by_email: null, user_id: null});
   const [errorMessage, setErrorMessage] = useState('');
   const [searching, setSearching] = useState(false);
   const [searchPerformed, setSearchPerformed] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
   const {signout} = useAuth();
   const navigate = useNavigate();
-  const filteredUsers = searchResult.filter(user => {
-    return user.toLowerCase().includes(searchQuery.toLowerCase())
-  })
 
   const handleSearch = async () => {
     try {
@@ -33,24 +31,34 @@ const BurgerMenu = ({active, setActive}) => {
         const data = await response.json();
         if (!data.error) {
           if (data.user_by_email) {
-            setSearchResult([data.user_by_email]);
-          } else {
-            setSearchResult(data.user_by_name);
+            setSearchResult({user_by_email: data.user_by_email, user_id: data.user_id});
           }
           setErrorMessage('');
         } else {
-          throw new Error(data.error || 'Failed to search users');
+          throw new Error(data.error, 'Failed to search users');
         }
       } else {
         throw new Error('Failed to search users');
       }
     } catch (error) {
       console.error(error);
-      setSearchResult([]);
-      setErrorMessage(error.message || 'No such user exists');
+      setSearchResult({});
+      setErrorMessage(error.message + ' No such user exists');
     } finally {
       setSearching(false);
     }
+  };
+
+
+  const handleUserClick = async (email, userId) => {
+    try {
+      setSelectedUser({email, userId});
+    } catch (error) {
+      console.error("Error fetching user ID:", error);
+    }
+  };
+
+  const initiateChat = (userId) => {
   };
 
   return (
@@ -64,11 +72,10 @@ const BurgerMenu = ({active, setActive}) => {
           <div className="flex flex-col mt-4">
             <input
               type="text"
-              placeholder="search by email or name"
+              placeholder="search by email"
               value={searchQuery}
               onChange={e => setSearchQuery(e.target.value)}
-              className="border border-gray-200 rounded p-2"
-            />
+              className="border border-gray-200 rounded p-2"/>
             <button onClick={handleSearch} className="bg-indigo-500 text-white font-semibold py-2 mt-2 rounded">
               {searching ? 'Searching...' : 'Search'}
             </button>
@@ -83,17 +90,21 @@ const BurgerMenu = ({active, setActive}) => {
               <span
                 className={`font-bold ${searchPerformed && searchResult.length === 0 ? 'no-users-found' : 'invisible'}`}>{searchResult.length > 0 ? `Users (${searchResult.length})` : 'No users found'}</span>
             </div>
-            <div className="flex flex-col space-y-1 mt-4 mx-2 max-h-72 overflow-y-auto scrollbar">
-              {filteredUsers.map((user, index) => (
-                <Link key={index} to={`/client/${index}`}>
+            <div className="flex flex-col space-y-1 mt-4 -mx-2 min-h-10 overflow-y-auto">
+
+              {searchResult.user_by_email && (
+                <Link to={`${searchResult.user_id}`}>
                   <button className="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2">
                     <div className="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full">
-                      {user.charAt(0)}
+                      {searchResult.user_by_email.charAt(0)}
                     </div>
-                    <div className="ml-2 text-sm font-semibold">{user}</div>
+                    <div className="ml-2 text-sm font-semibold">
+                      {searchResult.user_by_email} (id-{searchResult.user_id})
+                    </div>
                   </button>
                 </Link>
-              ))}
+              )}
+
             </div>
           </div>
         </div>
